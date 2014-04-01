64726f50a59b3c5d3ac04a0608a70fdfdf74c287
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-04-01 02:04:11 +0200

    indent

--- library/compiler/bansheeif.h
+++ library/compiler/bansheeif.h
@@ -5,7 +5,6 @@
 
 #include <core.h>
 #include <list.h>
-#include <cycboot.h>
 
 namespace BansheeIf
 {
@@ -15,17 +14,20 @@ namespace BansheeIf
 
   bool banshee_init ();
   void add_constant (string_t name, Absyn::cvartype_t c);
-  //void add_variable (Absyn::cvartype_t cvar);
+#if 0
+  void add_variable (Absyn::cvartype_t cvar);
+#endif
 
-  constraint_t equality_constraint (Absyn::cvartype_t, Absyn::cvartype_t);
+  constraint_t equality_constraint      (Absyn::cvartype_t, Absyn::cvartype_t);
   constraint_t cond_equality_constraint (Absyn::cvartype_t, Absyn::cvartype_t);
-  constraint_t inclusion_constraint (Absyn::cvartype_t, Absyn::cvartype_t);
-  constraint_t implication_constraint (constraint_t, constraint_t);
-  constraint_t and_constraint (constraint_t, constraint_t);
-  constraint_t or_constraint (constraint_t, constraint_t);
-  constraint_t not_constraint (constraint_t);
-  constraint_t cmpeq_constraint (Absyn::cvartype_t t1, Absyn::cvartype_t t2);
-  constraint_t check_constraint (constraint_t);
+  constraint_t inclusion_constraint     (Absyn::cvartype_t, Absyn::cvartype_t);
+  constraint_t implication_constraint   (constraint_t, constraint_t);
+  constraint_t and_constraint           (constraint_t, constraint_t);
+  constraint_t or_constraint            (constraint_t, constraint_t);
+  constraint_t not_constraint           (constraint_t);
+  constraint_t cmpeq_constraint         (Absyn::cvartype_t, Absyn::cvartype_t);
+  constraint_t check_constraint         (constraint_t);
+
   string_t constraint2string (constraint_t);
   constraint_t add_location (string_t<`H>, constraint_t);
   bool add_constraint (Absyn::seg_t, constraint_t);
@@ -37,8 +39,10 @@ namespace BansheeIf
   void reset_local_cvar (Absyn::type_t cvar);
   void register_toplevel_cvar (Absyn::type_t cvar);
   void init_toplevel_cvars (List::list_t<$(Absyn::type_t, List::list_t<constraint_t, `H>) @>);
+
   //resolution
   void resolve_or_default (Absyn::type_t t);
+
   //for statistics
   int add_all_constraints (List::list_t<$(Absyn::type_t, List::list_t<constraint_t, `H>) @>);
 }
